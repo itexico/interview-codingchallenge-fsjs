75e5f5271220e867b315ee39967453e5c6b4ac79
Luis Manuel Suarez <Luisma.suarez@hotmail.com>
2016-12-13 04:03:55 -0600

    added connection to mongodb
    I've added the post route to save the list item

---
+++ src/server/models/list
@@ -0,0 +1,8 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ListSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('List', ListSchema);
--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,11 @@
 // BASE SETUP
 // =============================================================================
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://admin:root@jello.modulusmongo.net:27017/upaDyh2u'); // connect to our database
+
+var List    = require('./models/list');
+
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
@@ -19,16 +24,49 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening.');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'welcome to the list api!' });
 });
 
 
-router.get('/bear', function(req, res) {
-    res.json({ message: 'this is a bear' });
+router.get('/list', function(req, res) {
+    res.json({ message: 'welcome to lists' });
 });
 // more routes for our API will happen here
+
+
+// on routes that end in /list
+// ----------------------------------------------------
+router.route('/list')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        console.log('got to lists');
+
+        var list = new List();      // create a new instance of the List model
+        list.name = req.body.name;  // set the list's name (comes from the request)
+        console.log('list name: '+list.name);
+
+        // save the list and check for errors
+        list.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'List '+list.name+' created!' });
+        });
+
+    });
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
@@ -37,4 +75,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('port is ' + port);
