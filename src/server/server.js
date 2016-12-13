90a2975c86921d932df57a0ec8119eb63c79db12
Luis Manuel Suarez <Luisma.suarez@hotmail.com>
2016-12-13 06:06:59 -0600

    added CR and D  for LISTS
    Fleshed out the API with routes for
    -adding new lists,
    -reading all lists
    -finding a list
    -removing a list
    Todo, add Items to lists

--- src/server/server.js
+++ src/server/server.js
@@ -2,11 +2,13 @@
 
 // BASE SETUP
 // =============================================================================
-
-var mongoose   = require('mongoose');
-mongoose.connect('mongodb://admin:root@jello.modulusmongo.net:27017/upaDyh2u'); // connect to our database
-
+var mongoose = require('mongoose');
 var List    = require('./models/list');
+var Item    = require('./models/item');
+
+mongoose.connect('mongodb://admin:root@jello.modulusmongo.net:27017/upaDyh2u');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 // call the packages we need
 var express    = require('express');        // call express
@@ -35,37 +37,63 @@ router.use(function(req, res, next) {
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'welcome to the list api!' });
+    res.json({ message: 'welcome to the iTexico Interview api!' });
 });
 
+// get the list with that name (accessed at GET http://localhost:8080/api/list/:name)
+//todo: find a way to add a item to the list
+router.route('/lists/:name')
+.get(function(req, res) {
+    List.find({ name: req.params.name },function(err, list) {
+        if (err)
+        res.send(err);
+        //var newItem=new Item({name:'star wars'}); // this is what id like to do
+        //list.items.push(9);  // how can i do this?
 
-router.get('/list', function(req, res) {
-    res.json({ message: 'welcome to lists' });
+        res.json({message:list.length+' List found',data:list});
+    });
+    // List.find({ name: /^pizza/ }, function(){});
+})
+//polish status code for deletion, maybe return deleted object?
+.delete(function(req, res) {
+    List.remove({ name: req.params.name },function(err) {
+        if (err)
+        res.send(err);
+        //if(list)
+        res.json({status:204})
+    });
 });
+
 // more routes for our API will happen here
 
 
 // on routes that end in /list
 // ----------------------------------------------------
-router.route('/list')
+router.route('/lists')
 
-    // create a bear (accessed at POST http://localhost:8080/api/bears)
-    .post(function(req, res) {
-        console.log('got to lists');
-
-        var list = new List();      // create a new instance of the List model
-        list.name = req.body.name;  // set the list's name (comes from the request)
-        console.log('list name: '+list.name);
-
-        // save the list and check for errors
-        list.save(function(err) {
-            if (err)
-                res.send(err);
-
-            res.json({ message: 'List '+list.name+' created!' });
-        });
-
+// create a list (accessed at POST http://localhost:8080/api/lists)
+.post(function(req, res) {
+    var list = new List({ name: req.body.name });
+    list.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('list '+list.name+'has been created');
+            res.json({ message: 'list '+list.name+'has been created',data:list });
+        }
     });
+})
+// get all lists (accessed at GET http://localhost:8080/api/lists)
+.get(function(req, res) {
+    List.find(function (err, lists) {
+        if (err) return console.error(err);
+        console.log(lists);
+        res.json({ message: 'welcome to lists',data:lists });
+    });
+
+
+
+});
 
 
 // REGISTER OUR ROUTES -------------------------------
