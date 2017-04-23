4754ef7241f24c9eb088d7ea65f628c669af4051
Daniel Rodríguez Monroy <willsnake87@gmail.com>
2017-04-23 15:22:25 -0500

    Added the files for the environment variables

---
+++ src/server/config/env/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  mongo: {
+    port: process.env.MONGO_PORT || 27017,
+    url: process.env.MONGO_URL || 'localhost',
+    db: process.env.MONGO_DATABASE || 'challenge',
+    user: process.env.MONGO_USER || '',
+    password: process.env.MONGO_PASSWORD || ''
+  }
+}
