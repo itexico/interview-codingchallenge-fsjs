f93bb7fd2c73dc3f368b0f831c099388d38f388c
Daniel Rodríguez Monroy <willsnake87@gmail.com>
2017-04-23 16:03:19 -0500

    Added the config files for the unitary tests
    Added the config files for the unitary tests

---
+++ src/server/test/bootstrap.test.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/**
+ * New code used to boot the configuration fo the server to the mocha tests
+ */
+const agent = require('supertest')
+const server = require('../app')
+
+before(function (done) {
+  process.env.NODE_ENV = 'test'
+
+  global.test = {
+    server: server,
+    agent: agent(process.env.TEST_URL || server.listen()),
+    token: '',
+    fixtures: {
+      users: []
+    }
+  }
+  done()
+})
