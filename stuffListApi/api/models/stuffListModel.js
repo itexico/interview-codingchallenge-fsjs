41bc51f9e96a59ba5e83b746e83eb8f27ddf8e77
Emigdio Torres Ruiz <emigdio821@gmail.com>
2017-07-21 21:10:58 -0500

    Initial commit <Created express API>
    Created API server using express and mongoose.

---
+++ stuffListApi/api/models/stuffListModel.js
@@ -0,0 +1,13 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var StuffSchema = new Schema({
+    name: {
+        type: String,
+        Required: 'Please enter some favorite stuff you love.'
+    }
+});
+
+module.exports = mongoose.model('Stuffs', StuffSchema);
