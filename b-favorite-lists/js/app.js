80b729aca4af6c816095d0ab600cf941a7cc5f6a
BereniceTovar <ybere_tovar@hotmail.com>
2018-09-17 22:45:45 -0500

    A way to view, add, edit, and delete items was added

--- b-favorite-lists/js/app.js
+++ b-favorite-lists/js/app.js
@@ -1,9 +1,6 @@
-
 //Function that obtain the name of list
 function getNameL () {
     var titleOfList = $('#nameOfList').val();
-    console.log(titleOfList);
-    
 
     addNameL(titleOfList);
     $('#nameOfList').val("");
@@ -11,13 +8,15 @@ function getNameL () {
 
 var cardTemplate = "<div class='card card-template'>" +
                         "<h5 class='card-header'>Favorite  __titleOfList__</h5>" +
-                            "<div class='card-body'>" +
-                                "<h5 class='card-title'>First note</h5>" +
-                                "<a href='#' class='btn btn-outline-info btn-add' data-toggle='modal' data-target='.add-item'>Add item</a>" +
-                                "<a href='#'><i class='fas fa-trash-alt'></i></a>" +
+                            "<div class='card-body' data-parent-type='father'>" +
+                                "<div class='card-title'></div>" +
+                                "<a href='#' class='btn btn-outline-info btn-add' data-toggle='modal' data-target='.add-item'>Add</a>" +
+                                "<a onClick='deleteItem()' class='delete-btn' href='#'><i class='fas fa-trash-alt'></i></a>" +
                             "</div>" + 
                     "</div>";
 
+
+//Function that fill template
 function addNameL (titleOfList) {
     var template = "";
     template = cardTemplate.replace(" __titleOfList__", titleOfList);
@@ -25,8 +24,37 @@ function addNameL (titleOfList) {
     containerLists.append(template);
 }
 
+//Function that obtains value of items in the fav list and adds the new item
+function valueItem () {
+    var newItem = $('.new-item').val();
+    $('.new-item').val('');
+    var contItem = document.createElement('h6');
+    contItem.append(newItem);
+
+    var generalContainer = $('.card-title');
+    generalContainer.append(contItem);
+
+    //Let to edit the content
+    $('h6').attr('contentEditable','true');
+}
+
+
+//Function that delete an item
+function deleteItem () {
+    var item = $(event.currentTarget);
+    var card = item.parent();
+    var containerCard = card.parent();
+    var generalContainer = containerCard.parent();
+
+    generalContainer.remove();
+}
+
 $(document).ready(function(){
     $('#createBtn').click (function(){
-    getNameL();
+        getNameL();
+    })
+
+    $('.add-btn').click (function(){
+        valueItem();
     })
 });
