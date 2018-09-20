bb009bc565adc4fe10f9eb96ced555257ae0189e
francia5 <francia.va.or@gmail.com>
2018-09-20 11:14:33 -0500

    changes are made to the file App.js and AddTask.js

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import AddTask from './components/AddTask';
-import Items from './components/Items';
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state= {
-      name:[],
-      items: []
-    }
-
-    this.writing = this.writing.bind(this);
-    this.list = this.list.bind(this);
-  }
-
-  writing(e){
-    this.setState({name:e.target.value});
-    console.log(e.target.value);
-  }
-
-  list(e){
-    e.preventDefault();
-    console.log("holi");
-    
-  }
-
-
-
+ 
   render() {
     return (
               <div className="row">
                 <div className="col-3">
-                    <AddTask onSubmit={this.list} onChange={this.writing}/>
-                    {/* <Items onSubmit={this.list} onKeyPress={this.writing} /> */}
+                    <AddTask/>
+
                 </div>
               </div>
     );
