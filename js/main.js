a7189688a8484a8e1d4582aa90531a478b4a7ce0
SarahWinchester <indira_ruli@hotmail.com>
2018-09-23 20:07:39 -0500

    Added New Items with Bug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-let globalId;
-
 //Saving info from Card
 function getCardData() {
   let title = $("#titleId").val();
@@ -25,8 +23,8 @@ function addCardData(title,comment,favorites) {
   let titleId= title.split(" ").join("-");
   console.log(titleId)
   //Card Template
- 
-
+  
+  
   let template = `
   <div class="card offset-1" style="width: 18rem;">
   <div class="card-body">
@@ -34,33 +32,55 @@ function addCardData(title,comment,favorites) {
   <p class="card-text">{{comment}}</p>
   </div>
   <ul class="list-group list-group-flush " id="{{dynamicId}}"  >
-  <li class="list-group-item">{{favorites}} <button class="btn text-right"><i class="fas fa-edit"></i></button><button class="btn text-right" onClick="deleteList()" ><i class="fas fa-eraser" ></i></button>  </li>
+  <li class="list-group-item">{{favorites}}
+  <button class="btn text-right">
+  <i class="fas fa-edit"></i>
+  </button>
+  <button class="btn text-right" onClick="deleteList()" >
+  <i class="fas fa-eraser" >
+  </i>
+  </button>  
+  </li>
   </ul>
   <!--Button for adding an item-->
-  <button href="#" class="btn btn-primary" data-toggle="modal" data-target="#modalAdd">Add an Item</button>
-  <button href="#" class="btn btn-secondary deleteList " onClick="deleteList()"   >Delete List</button>
-  
+  <button href="#" class="btn btn-primary itemAddition" onClick="retrieveData()" data-toggle="modal" data-target="#modalAdd">Add an Item</button>
+  <button href="#" class="btn btn-secondary deleteList " onClick="deleteList()" >Delete List</button>
   </div>
   `
   let finalTemplate = template.replace("{{title}}",title).replace("{{dynamicId}}",titleId).replace("{{comment}}",comment).replace("{{favorites}}",favorites);
   $("main").append(finalTemplate);
   swal("Good job!", "Lista Agregada!", "success");
-  globalId=titleId;
 }
 
 
 
 //retrieve data from new item
 function retrieveData() {
-  let addingFavorite= $("#addSecondFavorite").val();
+  let addingFavorite= $("#addNewItem").val();
   console.log(addingFavorite);
-  let templatelist = `<li class="list-group-item deleteItem">{{addingFavorite}} <button class="btn text-right"><i class="fas fa-edit"></i></button> <button class=" btn text-right data-toggle="modal" data-target="#modalAdd" onClick="deleteList()"  ><i class="fas fa-eraser" ></i></button> </li>
+  let templatelist = `<li class="list-group-item deleteItem">{{addingFavorite}}
+  <button class="btn text-right">
+  <i class="fas fa-edit">
+  </i>
+  </button> <button class=" btn text-right data-toggle="modal" data-target="#modalAdd" onClick="deleteList()"  >
+  <i class="fas fa-eraser"></i>
+  </button> 
+  </li>
   `
   let finalTemplateList = templatelist.replace("{{addingFavorite}}",addingFavorite);
   
-  
-  $("ul").append(finalTemplateList); 
-  $("#addSecondFavorite").val("");
+  let hearingItem = $(event.currentTarget);
+  let getchildren= hearingItem.parent();
+  let getUl= getchildren.children("ul");
+  let getId= $(getUl).attr("id");
+  console.log(getId)
+  $("#savingFavorite").click(function (e) { 
+    
+    $("#"+ getId).append(finalTemplateList); 
+    
+    
+  });
+  $("#addNewItem").val("");
   
 }
 
@@ -70,9 +90,11 @@ $("#savingChanges").click(function (e) {
   getCardData(); 
 });
 //hearing the click favorite
-$("#savingFavorite").click(function(e){
-  retrieveData();
-})
+// $("savingFavorite").click(function(e){
+//   console.log("escuchado aqui");
+
+//   retrieveData();
+// })
 //delete button
 function deleteList() {
   console.log("borrado");
