d9b0017e4b576a0f090b73a75a2469fcbd70523f
SarahWinchester <indira_ruli@hotmail.com>
2018-09-23 22:36:45 -0500

    click doesnt work

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-//Saving info from Card
-function getCardData() {
+//Values for card
+$("#savingChanges").click(function (e) { 
+  e.preventDefault();
   let title = $("#titleId").val();
   let comment= $("#commentId").val();
   let favorites= $("#addTaste").val();
   console.log(title, comment, favorites);
-  
-  
-  addCardData(title,comment,favorites); 
-  
+
+  if (title === "" || comment ===""|| favorites=="") {
+    alert("all inputs must be filled")
+  }else{
+    addCardData(title,comment,favorites); 
+    
+  }
   $("#titleId").val("");
   $("#commentId").val("");
   $("#addTaste").val("");
   
-}
-
-//Adding data
+  
+});
+//Adding data to card
 function addCardData(title,comment,favorites) {
   
-  console.log("aqui manda la data");
-  console.log(title);
-  console.log(comment);
-  console.log(favorites);
+  console.log("data moved");
+  
   let titleId= title.toLowerCase().split(" ").join("-");
   console.log(titleId)
-  //Card Template
-  
-  
+  //Card Template literal creating a dynamic Id from the title
   let template = `
   <div class="card offset-1" style="width: 18rem;">
   <div class="card-body">
@@ -44,58 +44,53 @@ function addCardData(title,comment,favorites) {
   </li>
   </ul>
   <!--Button for adding an item-->
-  <button href="#" class="btn btn-primary itemAddition" onClick="retrieveData()" data-toggle="modal" data-target="#modalAdd">Add an Item</button>
+  <button href="#" class="btn btn-primary " onClick="retrieveData()" data-toggle="modal" data-target="#modalAdd">Add an Item</button>
   <button href="#" class="btn btn-secondary deleteList " onClick="deleteList()" >Delete List</button>
   </div>
   `
   let finalTemplate = template.replace("{{title}}",title).replace("{{dynamicId}}",titleId).replace("{{comment}}",comment).replace("{{favorites}}",favorites);
   $("main").append(finalTemplate);
-  swal("Good job!", "Lista Agregada!", "success");
+  swal("Good job!", "Added List", "success");
 }
-
-
 
 //retrieve data from new item
-function retrieveData() {
-  let addingFavorite= $("#addNewItem").val();
-  console.log(addingFavorite);
-  let templatelist = `<li class="list-group-item deleteItem">{{addingFavorite}}
-  <button class="btn text-right">
-  <i class="fas fa-edit">
-  </i>
-  </button> <button class=" btn text-right data-toggle="modal" data-target="#modalAdd" onClick="deleteList()"  >
-  <i class="fas fa-eraser"></i>
-  </button> 
-  </li>
-  `
-  let finalTemplateList = templatelist.replace("{{addingFavorite}}",addingFavorite);
+$().click(function (e) { 
+  e.preventDefault();
   
-  let hearingItem = $(event.currentTarget);
-  let getchildren= hearingItem.parent();
-  let getUl= getchildren.children("ul");
-  let getId= $(getUl).attr("id");
+});
+function retrieveData() {
+
+  let addingItem= $("#addNewItem").val();
+  console.log(addingItem);
+  let hearingItem = $(event.currentTarget).parent().children("ul");
+  // let getchildren= hearingItem.parent();
+  // let getUl= getchildren.children("ul");
+  let getId= $(hearingItem).attr("id");
   console.log(getId)
-  $("#savingFavorite").click(function (e) { 
-
+  let templatelist =
+   `<li class="list-group-item">{{addingItem}}
+   <button class="btn text-right">
+   <i class="fas fa-edit"></i>
+   </button>
+   <button class="btn text-right" onClick="deleteList()" >
+   <i class="fas fa-eraser" >
+   </i>
+   </button>  
+   </li>
+  `
+  let finalTemplateList = templatelist.replace("{{addingItem}}",addingItem);
+ 
+  $("#savingFavorite").click(function () { 
+    
     $("#"+ getId).append(finalTemplateList); 
-      $("#addNewItem").val("");
-
+    
     
   });
-
+  
 }
 
-//saving button
-$("#savingChanges").click(function (e) { 
-  e.preventDefault();
-  getCardData(); 
-});
-//hearing the click favorite
-// $("savingFavorite").click(function(e){
-//   console.log("escuchado aqui");
-  
-//   retrieveData();
-// })
+
+
 //delete button
 function deleteList() {
   console.log("borrado");
