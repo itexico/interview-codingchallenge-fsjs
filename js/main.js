f94c63b4a0a8cf8efd4d658371dcb4ba8366215a
SarahWinchester <indira_ruli@hotmail.com>
2018-09-24 05:31:09 -0500

    ADDING ITEMS TO LIST

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+let idRescue="";
 //Values for card
 $("#savingChanges").click(function (e) { 
   e.preventDefault();
@@ -5,7 +6,7 @@ $("#savingChanges").click(function (e) {
   let comment= $("#commentId").val();
   let favorites= $("#addTaste").val();
   console.log(title, comment, favorites);
-
+  
   if (title === "" || comment ===""|| favorites=="") {
     alert("all inputs must be filled")
   }else{
@@ -44,7 +45,7 @@ function addCardData(title,comment,favorites) {
   </li>
   </ul>
   <!--Button for adding an item-->
-  <button href="#" class="btn btn-primary " onClick="retrieveData()" data-toggle="modal" data-target="#modalAdd">Add an Item</button>
+  <button href="#" class="btn btn-primary addItemClick " onClick="getIdTry()" data-toggle="modal" data-target="#modalAdd">Add an Item</button>
   <button href="#" class="btn btn-secondary deleteList " onClick="deleteList()" >Delete List</button>
   </div>
   `
@@ -52,54 +53,51 @@ function addCardData(title,comment,favorites) {
   $("main").append(finalTemplate);
   swal("Good job!", "Added List", "success");
 }
-
+function getIdTry() {
+  let hearingItem = $(event.currentTarget).parent().children("ul");
+  let getId=$(hearingItem).attr("id");
+  console.log(getId)
+  idRescue=getId;
+  
+}
 //retrieve data from new item
-
-function retrieveData() {
-
+$("#savingFavorite").click(function (e) { 
+  e.preventDefault();
+  console.log("entro");
   let addingItem= $("#addNewItem").val();
   console.log(addingItem);
-  let hearingItem = $(event.currentTarget).parent().children("ul");
-  // let getchildren= hearingItem.parent();
-  // let getUl= getchildren.children("ul");
-  let getId= $(hearingItem).attr("id");
-  console.log(getId)
   let templatelist =
-   `<li class="list-group-item">{{addingItem}}
-   <button class="btn text-right editItem">
-   <i class="fas fa-edit"></i>
-   </button>
-   <button class="btn text-right" onClick="deleteList()" >
-   <i class="fas fa-eraser" >
-   </i>
-   </button>  
-   </li>
+  `<li class="list-group-item">{{addingItem}}
+  <button class="btn text-right editItem">
+  <i class="fas fa-edit"></i>
+  </button>
+  <button class="btn text-right" onClick="deleteList()" >
+  <i class="fas fa-eraser" >
+  </i>
+  </button>  
+  </li>
   `
   let finalTemplateList = templatelist.replace("{{addingItem}}",addingItem);
- 
-  $("#savingFavorite").click(function () { 
-    
-    $("#"+ getId).append(finalTemplateList); 
-    
+    $("#"+ idRescue).append(finalTemplateList); 
+    $("#addNewItem").val(" ")
+}); 
+  
+  
+  $(".editItem").click(function (e) { 
+    e.preventDefault();
+    console.log("entre");
     
   });
   
-}
-$(".editItem").click(function (e) { 
-  e.preventDefault();
-  console.log("entre");
   
-});
-
-
-//delete button
-function deleteList() {
-  console.log("borrado");
-  let item = $(event.currentTarget);
-  let card= item.parent();
-  card.remove();
-}
-//Modal
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+  //delete button
+  function deleteList() {
+    console.log("borrado");
+    let item = $(event.currentTarget);
+    let card= item.parent();
+    card.remove();
+  }
+  //Modal
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
