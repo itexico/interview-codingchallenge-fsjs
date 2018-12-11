f86f97744432be012a3454c9df941a6b1e612737
kikiondo <kikisvv@hotmail.com>
2018-12-11 00:31:50 -0600

    Yeah :'0 Api Rest Interactio with the client side

--- src/server/app.js
+++ src/server/app.js
@@ -3,10 +3,17 @@ const express = require('express'),
   app = express(),
   router = require('./router')
 
+// https://enable-cors.org
+
 app
   .set('port', process.env.PORT)
   .use(express.urlencoded({extended:false})) // Code the data
   .use(express.json())
+  .use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin','*')
+    res.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-TypeError, Accept')
+    next()
+  })
   .use('/api', router) // The api's home 
 
 module.exports = app
