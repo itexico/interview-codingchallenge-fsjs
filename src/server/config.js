7bf462e0a96e7417b0a61120d3c4079f2dbba05c
kikiondo <kikisvv@hotmail.com>
2018-12-11 17:34:12 -0600

    New route to heroku

--- src/server/config.js
+++ src/server/config.js
@@ -5,4 +5,6 @@ process.env.PORT = process.env.PORT || 3000
 
 if (process.env.NODE_ENV === 'dev') {
   process.env.URL_DB = 'mongodb://127.0.0.1:27017/stuff' 
+} else {
+  process.env.URL_DB = 'mongodb://<dbuser>:<dbpassword>@ds019846.mlab.com:19846/stuff'
 }
