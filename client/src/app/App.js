b9ea677b40ceb196c7be55791cdd17c960f03f0b
Valencb <valenciablancalu@gmail.com>
2018-12-11 18:52:30 -0600

    mongodb coconexion and REST methods working

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -2,6 +2,59 @@ import React, { Component } from 'react';
 import {Navbar, NavbarBrand, Card, Button, CardTitle, CardText, Row, Col, Form, Input } from 'reactstrap';
 
 class App extends Component {
+constructor() {
+    super();
+    this.state= {
+        title:'',
+        description:[],
+        tasks: []
+    };
+    this.addTask= this.addTask.bind(this);
+    this.handleChange= this.handleChange.bind(this);
+}
+
+
+addTask(e){
+    fetch('./test', {
+        method: 'POST',
+        body: JSON.stringify(this.state),
+        headers : {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({title: this.state.title, description: ''});
+            this.fetchTasks();
+
+        })
+        .catch(err => console.error(err));
+    e.preventDefault();
+}
+componentDidMount(){
+    this.fetchTasks();
+}
+
+fetchTasks() {
+    fetch('./test')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            this.setState({tasks: data});
+        });
+
+}
+
+handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({
+        [name]: value
+    });
+};
+
     render(){
         return (
             <div className="container">
@@ -19,23 +72,45 @@ class App extends Component {
                         <Col sm="6">
                             <Card body>
                             <CardTitle className="d-flex justify-content-center">Lists of my favorite stuff!</CardTitle>
-                            <Form className="mb-3">
-                                <Input placeholder="Add your favorite stuff!" ></Input>
-                            </Form>
-                            <Button color="info">Go somewhere</Button>
+                                <Form onSubmit={this.addTask} className="mb-3">
+                                    <Input className="mb-2"  name="title" onChange={this.handleChange} text="text" placeholder="Your favorite stuff title!" value={this.state.title}></Input>
+                                    <textarea name="description" onChange={this.handleChange} placeholder="Add your favorite stuff!" value={this.state.description}></textarea>
+                                    <Button type="submit">
+                                    <i className="fas fa-plus-circle"> Add</i>
+                                    </Button>
+                                    <Button type="submit" color="info">
+                                    <i className="fas fa-check-circle"> Save</i>
+                                    </Button>
+                                    <Button>
+                                    <i className="fas fa-edit"></i>
+                                    </Button>
+                                </Form>
                             </Card>
                         </Col>
                         <Col sm="6">
                             <Card body>
-                            <CardTitle>Special Title Treatment</CardTitle>
-                            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                            <Button color="info" >Go somewhere</Button>
+                            <CardTitle >Special Title Treatment</CardTitle>
+                                    {
+                                        this.state.tasks.map(task => {
+                                            return (
+                                                <tr key={task._id}>
+                                                    <td>{task.title}</td>
+                                                    <td>{task.description}</td>
+                                                    <td>
+                                                    <i className="fas fa-minus-circle"></i>
+                                                    <i className="fas fa-plus-circle"></i>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                <Button color="info" >Go somewhere</Button>
                             </Card>
                         </Col>
                     </Row> 
                 </main> 
             </div>
-        );
+            );
     };
 };
 
