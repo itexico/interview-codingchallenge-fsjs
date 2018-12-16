95cf92b22a7e5d8f74414e27dad8e9583903b89c
Anuar Jimenez <heyitsanuar@gmail.com>
2018-12-16 14:49:15 -0600

    Add modification to routes.

--- src/server/routes/ListRouter.js
+++ src/server/routes/ListRouter.js
@@ -4,9 +4,9 @@ const AuthMiddleware = require('../middlewares/authentication');
 let api              = express.Router();
 
 api.get('/list/:id', AuthMiddleware.ensureAuth, ListController.getList);
-api.get('/lists', AuthMiddleware.ensureAuth, ListController.getLists);
 api.post('/list', AuthMiddleware.ensureAuth, ListController.saveList);
 api.put('/list/:id', AuthMiddleware.ensureAuth, ListController.updateList);
 api.delete('/list/:id', AuthMiddleware.ensureAuth, ListController.removeList);
+api.get('/lists', AuthMiddleware.ensureAuth, ListController.getLists);
 
 module.exports = api;
