954449ce103728a2ad0a0deec76ed93d94af15b1
Luis Jose Aguilar Espinosa <lesp@lesp-macbook.local>
2020-05-19 21:18:35 -0500

    Add List schema and Create handler

--- src/server/controllers/list/index.js
+++ src/server/controllers/list/index.js
@@ -1,19 +1,25 @@
 import { Router } from "express";
-
-const lists = [
-  { id: 1, name: "books" },
-  { id: 2, name: "movies" },
-  { id: 3, name: "food" },
-];
+import { List } from "../../models/List";
+import { Types } from "mongoose";
 
 const router = Router();
 
-// Get all lists
+/**
+ * Endpoints
+ *
+ * GET /lists - Retrieve all lists
+ * GET /lists/:id - Retrieve a list by id
+ * POST /lists - Create a new list
+ * PUT /lists/:id - Update a list by id
+ * DELETE /lists/:id - Delete a list by id
+ * */
+
+// GET /lists - Retrieve all lists
 router.get("/", (req, res) => {
   res.json({ lists });
 });
 
-// Get list by id
+// GET /lists/:id - Retrieve a list by id
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   const list = lists.find((list) => list.id === Number(id));
@@ -22,6 +28,29 @@ router.get("/:id", (req, res) => {
     res.json({ list });
   } else {
     res.status(404).json({ message: "The list requested is not found" });
+  }
+});
+
+// POST /lists - Create a new list
+router.post("/", async (req, res) => {
+  const listId = Types.ObjectId();
+
+  const list = new List({
+    _id: listId,
+    title: req.body.title,
+    items: [],
+  });
+
+  try {
+    await list.save();
+    res.status(201).json({
+      message: `You've successfully created a new list`,
+      list,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
   }
 });
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
 import { route } from "./controllers";
 
 const PORT = 3000;
+const DB_USER = "developer";
+const DB_PASSWORD = "developer2020";
+
+mongoose.connect(
+  `mongodb://${DB_USER}:${DB_PASSWORD}@ds257564.mlab.com:57564/itexico-interview-cc`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const app = express();
 
+app.use(bodyParser.json());
 route(app);
 
 app.listen(PORT, () => `Itexico API on http://localhost:${PORT}`);
