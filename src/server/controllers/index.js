c99bea35d3405a5f81c83eb95d5d165fbb62989d
Luis Jose Aguilar Espinosa <lesp@lesp-macbook.local>
2020-05-20 05:43:26 -0500

    Add Item Read by Id handler

--- src/server/controllers/index.js
+++ src/server/controllers/index.js
@@ -1,7 +1,9 @@
 import { listRouter } from "./list";
+import { itemsRouter } from "./item";
 
 export const route = (app) => {
   app.use("/lists", listRouter);
+  app.use("/items", itemsRouter);
 
   app.use((error, req, res, next) => {
     const defaultErrorMessage = "Something went wrong. Please try again.";
--- src/server/controllers/item/index.js
+++ src/server/controllers/item/index.js
@@ -1,16 +1,29 @@
 import { Router } from "express";
+import { Item } from "../../models/Item";
 
-const lists = [
-  { id: 1, name: "books" },
-  { id: 2, name: "movies" },
-  { id: 3, name: "food" },
-];
+/**
+ * Endpoints
+ *
+ * GET /items/:itemId - Retrieve an item from a list
+ * PATCH /items/:itemId - Update a list by id
+ * DELETE /items/:itemId - Delete a list by id
+ * */
 
 const router = Router();
 
-// Get all lists
-router.get("/", (req, res) => {
-  res.json({ lists });
+// GET /items/:itemId - Retrieve an item from a list
+router.get("/:itemId", async (req, res, next) => {
+  const { itemId } = req.params;
+
+  try {
+    const item = await Item.findById(itemId).exec();
+
+    if (!item) return res.status(404).json({ item: null });
+
+    res.status(200).json({ item });
+  } catch (error) {
+    next(error);
+  }
 });
 
 // Get list by id
@@ -25,4 +38,4 @@ router.get("/:id", (req, res) => {
   }
 });
 
-export const listRouter = router;
+export const itemsRouter = router;
