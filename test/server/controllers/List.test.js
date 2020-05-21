bbf4fb93229916fcb36967beda4d49317cfaa28a
Luis Jose Aguilar Espinosa <lesp@lesp-macbook.local>
2020-05-20 21:21:07 -0500

    Fix List unit tests

--- test/server/controllers/List.test.js
+++ test/server/controllers/List.test.js
@@ -2,7 +2,10 @@ import request from "supertest";
 import { connectDB, disconnectDB } from "../../../src/server/database";
 import app from "../../../src/server/app";
 
-const mockLists = [{ title: "My first list" }];
+const mockLists = [];
+
+const nonExistentId = "523510229168172350112114";
+const invalidId = "123";
 
 describe("List Model Operations", () => {
   beforeAll(async () => {
@@ -22,22 +25,22 @@ describe("List Model Operations", () => {
   });
 
   it("should create a new list", async () => {
-    const response = await request(app).post("/lists").send(mockLists[0]);
+    const response = await request(app)
+      .post("/lists")
+      .send({ title: "My first list" });
     const { list } = response.body;
 
     expect(response.statusCode).toBe(201);
 
     expect(list).toHaveProperty("listId");
-    expect(list).toHaveProperty("title", mockLists[0].title);
+    expect(list).toHaveProperty("title", "My first list");
     expect(list).toHaveProperty("items", 0);
 
-    mockLists[0] = list;
+    mockLists.push(list);
   });
 
   it("should error if title is not sent in creation properly", async () => {
-    const undefinedResponse = await request(app)
-      .post("/lists")
-      .send({ title: undefined });
+    const undefinedResponse = await request(app).post("/lists").send({});
 
     const nullResponse = await request(app)
       .post("/lists")
@@ -49,40 +52,27 @@ describe("List Model Operations", () => {
       .post("/lists")
       .send({ title: 0 });
 
-    const zeroStringResponnse = await request(app)
-      .post("/lists")
-      .send({ title: "0" });
-
     expect(undefinedResponse.statusCode).toBe(400);
     expect(nullResponse.statusCode).toBe(400);
     expect(emptyResponse.statusCode).toBe(400);
     expect(nonStringResponse.statusCode).toBe(400);
-    expect(zeroStringResponnse.statusCode).toBe(201);
 
     expect(undefinedResponse.body).toHaveProperty("error");
     expect(nullResponse.body).toHaveProperty("error");
     expect(emptyResponse.body).toHaveProperty("error");
     expect(nonStringResponse.body).toHaveProperty("error");
-
-    expect(zeroStringResponnse.body).toHaveProperty("list");
-
-    mockLists.push(zeroStringResponnse.body.list);
   });
 
-  it("should fetch 2 created lists", async () => {
+  it("should fetch the created lists", async () => {
     const response = await request(app).get("/lists");
     const { lists } = response.body;
 
     expect(response.statusCode).toBe(200);
-    expect(lists.length).toBe(2);
+    expect(lists.length).toBe(1);
 
     expect(lists[0]).toHaveProperty("listId");
-    expect(lists[0].title).toBe(mockLists[0].title);
+    expect(lists[0].title).toBe("My first list");
     expect(lists[0].items).toBe(0);
-
-    expect(lists[1]).toHaveProperty("listId");
-    expect(lists[1].title).toBe("0");
-    expect(lists[1].items).toBe(0);
   });
 
   it("should update the list title", async () => {
@@ -103,7 +93,7 @@ describe("List Model Operations", () => {
   it("should error if title is not sent in update properly", async () => {
     const undefinedResponse = await request(app)
       .patch(`/lists/${mockLists[0].listId}`)
-      .send({ title: undefined });
+      .send({});
 
     const nullResponse = await request(app)
       .patch(`/lists/${mockLists[0].listId}`)
@@ -115,36 +105,33 @@ describe("List Model Operations", () => {
 
     const nonStringResponse = await request(app)
       .patch(`/lists/${mockLists[0].listId}`)
-      .send({ title: 0 });
-
-    const zeroStringResponnse = await request(app)
-      .patch(`/lists/${mockLists[0].listId}`)
-      .send({ title: "0" });
+      .send({ title: 99 });
 
     expect(undefinedResponse.statusCode).toBe(400);
     expect(nullResponse.statusCode).toBe(400);
     expect(emptyResponse.statusCode).toBe(400);
     expect(nonStringResponse.statusCode).toBe(400);
-    expect(zeroStringResponnse.statusCode).toBe(200);
 
     expect(undefinedResponse.body).toHaveProperty("error");
     expect(nullResponse.body).toHaveProperty("error");
     expect(emptyResponse.body).toHaveProperty("error");
     expect(nonStringResponse.body).toHaveProperty("error");
-
-    expect(zeroStringResponnse.body).toHaveProperty("list");
-
-    mockLists.push(zeroStringResponnse.body.list);
   });
 
-  it("should error updating the list if id does not exist", async () => {
-    const response = await request(app)
-      .patch(`/lists/123511822291147113225734`)
+  it("should error updating the list with invalid id", async () => {
+    const nonExistentIdResponse = await request(app)
+      .patch(`/lists/${nonExistentId}`)
       .send({ title: "My non existent list" });
 
-    expect(response.statusCode).toBe(404);
+    const invalidIdResponse = await request(app)
+      .patch(`/lists/${invalidId}`)
+      .send({ title: "My non existent list" });
 
-    expect(response.body).toHaveProperty("error");
+    expect(invalidIdResponse.statusCode).toBe(500);
+    expect(nonExistentIdResponse.statusCode).toBe(404);
+
+    expect(invalidIdResponse.body).toHaveProperty("error");
+    expect(nonExistentIdResponse.body).toHaveProperty("error");
   });
 
   it("should fetch a list by id without populated items", async () => {
@@ -154,14 +141,16 @@ describe("List Model Operations", () => {
     expect(response.statusCode).toBe(200);
 
     expect(list.listId).toBe(mockLists[0].listId);
-    expect(list.title).toBe("0");
+    expect(list.title).toBe("My first list updated");
     expect(list.items).toEqual(0);
   });
 
-  it("should error when fetching a list with invalid or not-found id", async () => {
-    const invalidObjectIdResponse = await request(app).get(`/lists/123`);
+  it("should error when fetching a list with invalid id", async () => {
+    const invalidObjectIdResponse = await request(app).get(
+      `/lists/${invalidId}`
+    );
     const notFountIdResponse = await request(app).get(
-      `/lists/123511822291147113225734`
+      `/lists/${nonExistentId}`
     );
 
     expect(notFountIdResponse.statusCode).toBe(404);
@@ -172,20 +161,22 @@ describe("List Model Operations", () => {
   });
 
   it("should delete a list by id", async () => {
-    const response = await request(app).delete(`/lists/${mockLists[1].listId}`);
+    const response = await request(app).delete(`/lists/${mockLists[0].listId}`);
     const { list } = response.body;
 
     expect(response.statusCode).toBe(200);
 
-    expect(list.listId).toBe(mockLists[1].listId);
-    expect(list.title).toBe(mockLists[1].title);
-    expect(list.items).toEqual(mockLists[1].items);
+    expect(list.listId).toBe(mockLists[0].listId);
+    expect(list.title).toBe("My first list updated");
+    expect(list.items).toEqual(0);
   });
 
   it("should error when deleting a list with invalid or not-found id", async () => {
-    const invalidObjectIdResponse = await request(app).delete(`/lists/123`);
+    const invalidObjectIdResponse = await request(app).delete(
+      `/lists/${invalidId}`
+    );
     const notFountIdResponse = await request(app).delete(
-      `/lists/123511822291147113225734`
+      `/lists/${nonExistentId}`
     );
 
     expect(notFountIdResponse.statusCode).toBe(404);
