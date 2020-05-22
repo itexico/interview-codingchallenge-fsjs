dfed8de1de558e73988e3b35445c406942e5f6ea
Luis Jose Aguilar Espinosa <lesp@lesp-macbook.local>
2020-05-22 03:54:41 -0500

    Add .env files for client and server and scripts

--- src/server/database.js
+++ src/server/database.js
@@ -3,17 +3,14 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 
 export const connectDB = () =>
   new Promise((resolve, reject) => {
-    const { NODE_ENV, DB_USER, DB_PASSWORD } = process.env;
+    const { NODE_ENV, MONGO_URI } = process.env;
 
     if (NODE_ENV !== "test") {
       mongoose
-        .connect(
-          `mongodb://${DB_USER}:${DB_PASSWORD}@ds257564.mlab.com:57564/itexico-interview-cc`,
-          {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-          }
-        )
+        .connect(`${MONGO_URI}`, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
         .then((_, error) => (error ? reject(error) : resolve()));
     } else {
       const mockDB = new MongoMemoryServer();
